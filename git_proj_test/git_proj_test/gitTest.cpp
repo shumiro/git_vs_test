0f54ce9127e0d3d9b91d186a85ca03ee1f230613
shumiro <shumiro@o.ncu.ac.kr>
2023-05-28 04:14:45 +0900

    update src

---
+++ git_proj_test/git_proj_test/gitTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+	return 0;
+}
